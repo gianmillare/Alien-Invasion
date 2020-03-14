a24834bdc4b9093476e6ba7d56beca261d18efd1
gianmillare <gianmillaregsm@gmail.com>
2020-03-14 15:40:40 -0700

    created the functions that will position the score on the screen and then display it on the screen

--- alien_invasion/scoreboard.py
+++ alien_invasion/scoreboard.py
@@ -16,3 +16,17 @@ class Scoreboard:
 
         # Prepare initial score
         self.prep_score()
+
+    def prep_score(self):
+        """Render the score into an image"""
+        score_str = str(self.stats.score)
+        self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
+
+        # Display the score at the top right of the screen
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+
+    def show_score(self):
+        """Draw the score on the screen"""
+        self.screen.blit(self.score_image, self.score_rect)
