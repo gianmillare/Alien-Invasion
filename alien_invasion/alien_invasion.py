19e19cd70b263134b22e4b2256be101094de0a4d
gianmillare <gianmillaregsm@gmail.com>
2020-03-14 15:07:31 -0700

    once the player restarts a fresh game, the difficulty will reset back to initial values

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -74,6 +74,10 @@ class AlienInvasion:
         """Start a new game when the player clicks play"""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+
+            # Reset the game settings
+            self.settings.initialize_dynamic_settings()
+
             # Reset the game stats
             self.stats.reset_stats()
             self.stats.game_active = True
