39cba2fa52eea2862eaa47bf2daebed17ff0c3d8
gianmillare <gianmillaregsm@gmail.com>
2020-03-13 20:11:41 -0700

    created the bullet.py file that will create the bullets fired from the ship.

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -9,5 +9,11 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship Settings
-        self.ship_speed = 1.5
+        self.ship_speed = 10.5
+
+        # Bullet Settings
+        self.bullet_speed = 15.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
 
